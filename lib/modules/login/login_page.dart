27e94119325959c792dee560dede16b16ba5954e
dan7sc <su7cheng@gmail.com>
2021-07-08 12:23:07 -0300

    Change body to Align in LoginPage

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -13,28 +13,31 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Form(
-              child: Column(
-                children: [
-                  InputTextWidget(
-                    label: "Email",
-                    validator: (value) => value!.isEmpty ? "Vazio" : null,
-                    onChanged: (value) {},
-                  ),
-                  InputTextWidget(
-                    label: "Password",
-                    obscureText: true,
-                    validator: (value) => value!.isEmpty ? "Vazio" : null,
-                    onChanged: (value) {},
-                  ),
-                ],
+      body: Align(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                child: Column(
+                  children: [
+                    InputTextWidget(
+                      label: "Email",
+                      validator: (value) => value!.isEmpty ? "Vazio" : null,
+                      onChanged: (value) {},
+                    ),
+                    InputTextWidget(
+                      label: "Password",
+                      obscureText: true,
+                      validator: (value) => value!.isEmpty ? "Vazio" : null,
+                      onChanged: (value) {},
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
